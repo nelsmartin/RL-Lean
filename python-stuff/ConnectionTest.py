e689a13152a7a609b28e627b520b200aa7d77706
Nels Martin <nels@nelsmartin.com>
2026-03-17 14:17:52 -0700

    added first theorems

--- python-stuff/ConnectionTest.py
+++ python-stuff/ConnectionTest.py
@@ -1,6 +1,7 @@
 import json
 from pantograph.server import Server
 from pathlib import Path
+from lean_dojo_v2.database import DynamicDatabase
 
 def build_so_tactic(
         close: list[str] | None = None,
@@ -25,5 +26,19 @@ oracle_tactic = build_so_tactic(
 
 server = Server(
     project_path=str(Path(__file__).parent.parent / "lean-stuff"),
-    imports=["Init", "LeanStuff.OracleTactic"],
+    imports=["Init", "LeanStuff.OracleTactic"]
 )
+
+goal_expr = "∀ n : Nat, 0 + n = n"
+initial_state = server.goal_start(goal_expr)
+initial_state = server.goal_tactic(initial_state, "intro h")
+
+
+suggestion_state = server.goal_tactic(initial_state, oracle_tactic)
+for msg in suggestion_state.messages:
+        data = json.loads(msg.data)
+        for move in data.get("nextMoves", []):
+            print(move)
+
+
+path = "/lean-stuff"
